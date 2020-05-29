5bac155076c0ddb7cf794b676c04a8219e58da10
YuJigun <yoozizix@naver.com>
2020-05-29 14:00:44 +0900

    assignment5of10

--- app/src/main/java/com/example/assignment7/DetailFragment.java
+++ app/src/main/java/com/example/assignment7/DetailFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+
 public class DetailFragment extends Fragment {
 
 
